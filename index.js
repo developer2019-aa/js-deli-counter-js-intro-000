a62c625e9d22c7b814756f03c75dca9ce4224616
Allen <developer2019@icloud.com>
2020-05-09 03:27:14 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function takeANumber(currentLine, newName) {
+  return `Welcome, ${newName}. You are number ${currentLine} in line.`
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length <= 0) {
+    return 'There is nobody waiting to be served!';
+  } else {
+    return katzDeliLine.shift();
+  }
+}
+
+function currentLine(array) {
+  if (array.length === 0) return 'The line is currently empty.';
+  let currentLine = 'The line is currently: ';
+
+  for (let i = 0; i < array.length; i++) {
+    if (i === array.length - 1) {
+        currentLine += `${i+1}. ${array[i]}`;
+    } else {
+        currentLine += `${i+1}. ${array[i]}, `;
+    }
+  }
+  return currentLine;
+}
